1e0e25b761ad9d17cedd9e427087f1000b78f132
Bob Rudis <bob@rud.is>
2016-08-16 02:08:24 +0000

    version 0.2.0

---
+++ R/aaa.r
@@ -0,0 +1,1 @@
+extensions <- mime_type <- response <- encoding <- NULL
---
+++ R/wand-package.r
@@ -0,0 +1,24 @@
+#' Retrieve 'Magic' Attributes from Files and Directories
+#'
+#' The 'libmagic' library provides functions to determine 'MIME' type and other
+#' metadata from files through their "magic" attributes. This is useful when you
+#' do not wish to rely solely on the honesty of a user or the extension on a
+#' file name. It also incorporates other metadata from the mime-db database
+#' <https://github.com/jshttp/mime-db>
+#'
+#' Based on \code{file} / \code{libmagic} - \url{https://github.com/file/file}
+#'
+#' @name wand
+#' @docType package
+#' @author Bob Rudis (@@hrbrmstr)
+#' @import purrr
+#' @import tibble
+#' @import tidyr
+#' @import stringi
+#' @importFrom rappdirs user_cache_dir
+#' @useDynLib wand
+#' @importFrom Rcpp sourceCpp
+#' @importFrom utils unzip
+#' @importFrom dplyr mutate left_join mutate_all
+#' @import stats
+NULL
